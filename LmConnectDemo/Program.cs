6d86a36936923ffb1d059001d3b98182598e756e
jpardeck <jon.pardeck@systematicgroup.com>
2022-05-10 13:54:17 -0500

    Updated to lastet Nuget package.

--- LmConnectDemo/Program.cs
+++ LmConnectDemo/Program.cs
@@ -110,25 +110,43 @@ namespace LmConnectDemo
                             // Step 4: Register for device changed events - optional
                             device.StateChangedEvent += Device_StateChangedEvent;
 
-                            // Step 5: Register for shot events
+                            // Step 5: Register for configuration changed events - optional
+                            device.ConfigurationChangedEvent += Device_ConfigurationChangedEvent;
+
+                            // Step 6: Register for shot events
                             device.ShotEvent += Device_ShotEvent;
+
+                            // Step 7: Register for shot video events - optional
+                            device.VideoAvailableEvent += Device_VideoAvailableEvent;
                             
                             try
                             {
-                                // Step 6: Connect to one or more launch monitors
+                                // Step 8: Connect to one or more launch monitors
                                 await device.Connect();
 
-                                // Step 7: Set configuration if desired - optional
+                                Console.WriteLine("Device Information");
+                                Console.WriteLine("  Software Version: {0}", device.DeviceInformation.SoftwareVersion);
+                                Console.WriteLine("  Firmware Version: {0}", device.DeviceInformation.FirmwareVersion);
+                                Console.WriteLine("  Hardware Revision: {0}", device.DeviceInformation.HardwareRevision);
+                                Console.WriteLine("  Serial Number: {0}", device.DeviceInformation.SerialNumber);
+                                Console.WriteLine("  Model Number: {0}", device.DeviceInformation.ModelNumber);
+
+                                // Step 9: Set configuration if desired - optional
                                 // Set the device to auto-arm as opposed to SDK arming it
                                 await device.SetConfiguration(ConfigurationId.AutoArm, true);
 
-                                // Set the club type to driver
-                                await device.SetConfiguration(ConfigurationId.Club, Club.ClubDriver);
+                                // Step 10: Set the club type to driver
+                                await device.SetConfiguration(ConfigurationId.Club, Club.Driver);
+
+                                // Step 11: Set session related configuration - optional
+                                await device.SetConfiguration(ConfigurationId.Location, Location.Screen);
+                                await device.SetConfiguration(ConfigurationId.Altitude, 1100);
+                                await device.SetConfiguration(ConfigurationId.Temperature, 70.0);
 
                                 // If we connected to the device, wait for a few shots
                                 await _complete.WaitAsync();
 
-                                // Step 8: Disconnect the device
+                                // Step 12: Disconnect the device
                                 await device.Disconnect();
                             }
                             catch (ConnectionException ex)
@@ -151,10 +169,20 @@ namespace LmConnectDemo
                     Console.WriteLine("Not authorized");
                 }
             }
-            catch (NotAuthorizedException)
+            catch (Exception)
             {
                 Console.WriteLine("Could not initialize. Not authorized");
             }
+        }
+
+        private static void Device_ConfigurationChangedEvent(object sender, ConfigurationChangedEventArgs e)
+        {
+            Console.WriteLine("Configuration changes, ID: {0}, Value: {1}", e.Id, e.Value);
+        }
+
+        private static void Device_VideoAvailableEvent(object sender, VideoAvailableEventArgs e)
+        {
+            Console.WriteLine("Video available  {0}", e.Url);
         }
 
         private static void Device_StateChangedEvent(object sender, StateChangedEventArgs e)
@@ -165,28 +193,27 @@ namespace LmConnectDemo
         static void Device_ShotEvent(object sender, ShotEventArgs e)
         {
             Console.WriteLine("Shot received - Type: {0}", e.Type);
+            Console.WriteLine("  Shot Id: {0}", e.Shot.Id);
+            Console.WriteLine("  Device Id: {0}", e.Shot.DeviceId);
+            Console.WriteLine("  Timestamp: {0}", e.Shot.Timestamp.ToLocalTime());
+            if (e.Shot.AttackAngle.HasValue) Console.WriteLine("  Attack Angle: {0:0.00}", e.Shot.AttackAngle);
+            if (e.Shot.BallSpeed.HasValue) Console.WriteLine("  Ball Speed: {0:0.00}", e.Shot.BallSpeed);
+            if (e.Shot.ClubPath.HasValue) Console.WriteLine("  Club Path: {0:0.00}", e.Shot.ClubPath);
+            if (e.Shot.ClubSpeed.HasValue) Console.WriteLine("  Club Speed: {0:0.00}", e.Shot.ClubSpeed);
+            if (e.Shot.FaceAngle.HasValue) Console.WriteLine("  Face Angle: {0:0.00}", e.Shot.FaceAngle);
+            if (e.Shot.HorizontalLaunchAngle.HasValue) Console.WriteLine("  Horizontal Launch Angle: {0:0.00}", e.Shot.HorizontalLaunchAngle);
+            if (e.Shot.SmashFactor.HasValue) Console.WriteLine("  Smash Factor: {0:0.00}", e.Shot.SmashFactor);
 
-            // Only print full shot data
             if (e.Type == ShotType.Flight)
             {
-                Console.WriteLine("  Shot Id: {0}", e.Shot.Id);
-                Console.WriteLine("  Device Id: {0}", e.Shot.DeviceId);
-                Console.WriteLine("  Timestamp: {0}", e.Shot.Timestamp.ToLocalTime());
-                Console.WriteLine("  Attack Angle: {0:0.00}", e.Shot.AttackAngle);
-                Console.WriteLine("  Ball Speed: {0:0.00}", e.Shot.BallSpeed);
-                Console.WriteLine("  Club Path: {0:0.00}", e.Shot.ClubPath);
-                Console.WriteLine("  Club Speed: {0:0.00}", e.Shot.ClubSpeed);
-                Console.WriteLine("  Face Angle: {0:0.00}", e.Shot.FaceAngle);
-                Console.WriteLine("  Horizontal Launch Angle: {0:0.00}", e.Shot.HorizontalLaunchAngle);
-                Console.WriteLine("  Smash Factor: {0:0.00}", e.Shot.SmashFactor);
-                Console.WriteLine("  Spin Axis: {0:0.00}", e.Shot.SpinAxis);
-                Console.WriteLine("  Spin rate: {0:0.00}", e.Shot.SpinRate);
-                Console.WriteLine("  Vertical Launch Angle: {0:0.00}", e.Shot.VerticalLaunchAngle);
-                Console.WriteLine("  Apex: {0:0.00}", e.Shot.Apex);
-                Console.WriteLine("  Carry Distance: {0:0.00}", e.Shot.CarryDistance);
-                Console.WriteLine("  Side: {0:0.00}", e.Shot.Side);
-                Console.WriteLine("  Side Total: {0:0.00}", e.Shot.SideTotal);
-                Console.WriteLine("  Total Distance: {0:0.00}", e.Shot.TotalDistance);
+                if (e.Shot.SpinAxis.HasValue) Console.WriteLine("  Spin Axis: {0:0.00}", e.Shot.SpinAxis);
+                if (e.Shot.SpinRate.HasValue) Console.WriteLine("  Spin rate: {0:0.00}", e.Shot.SpinRate);
+                if (e.Shot.VerticalLaunchAngle.HasValue) Console.WriteLine("  Vertical Launch Angle: {0:0.00}", e.Shot.VerticalLaunchAngle);
+                if (e.Shot.Apex.HasValue) Console.WriteLine("  Apex: {0:0.00}", e.Shot.Apex);
+                if (e.Shot.CarryDistance.HasValue) Console.WriteLine("  Carry Distance: {0:0.00}", e.Shot.CarryDistance);
+                if (e.Shot.Side.HasValue) Console.WriteLine("  Side: {0:0.00}", e.Shot.Side);
+                if (e.Shot.SideTotal.HasValue) Console.WriteLine("  Side Total: {0:0.00}", e.Shot.SideTotal);
+                if (e.Shot.TotalDistance.HasValue) Console.WriteLine("  Total Distance: {0:0.00}", e.Shot.TotalDistance);
 
                 // Track shots received and release semaphore once desired number has been seen
                 // so program can exit
